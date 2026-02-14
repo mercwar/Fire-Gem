8ee2d2f5adb495647e9a569aabf798a4767d348b
CVBGOD <mercwar01@gmail.com>
2026-02-14 09:29:34 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -13,15 +13,16 @@ nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
 # 2. LINK CORE
 ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
 
-# 3. RECURSIVE EXTENSION LOOP
+# 3. RECURSIVE LOOP
+echo "[AVIS-SH] Loading Extensions..."
 for f in "VERSION 1"/*.asm; do
     name=$(basename "$f" .asm)
     if [[ "$name" != "fire-gem" && "$name" != "fire-log" ]]; then
+        echo "[AVIS-SH] Forging Extension: $name"
         nasm -f elf64 "$f" -o "VERSION 1/$name.o"
-        # LINK ONLY TO LOG TO SATISFY DUMB LINKER
+        # LINK ONLY TO LOG
         ld "VERSION 1/$name.o" "VERSION 1/fire-log.o" -o "VERSION 1/$name.exe"
     fi
 done
 
 "./VERSION 1/fire-gem.exe"
-
