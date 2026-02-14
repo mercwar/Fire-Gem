476ca6604b22e1fb6d203a1b6485037e89ceb2fb
CVBGOD <mercwar01@gmail.com>
2026-02-13 20:52:16 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -2,19 +2,22 @@
 # VERSION 1/fire-gem.sh
 set -e
 
-# Compile the core first
+# 1. Forge the Master Objects
 nasm -f elf64 "VERSION 1/fire-log.asm" -o "VERSION 1/fire-log.o"
 nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
+
+# 2. THE LINKER STRIKE
+# Fuses the Voice and Brain into the Version 1 Executive
 ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
 
-# Compile everything else in the Version 1 sector
+# 3. Forge the remaining Chain Artifacts (Seed, Spec, FVS)
 for f in "VERSION 1"/*.asm; do
     name=$(basename "$f" .asm)
-    if [ "$name" != "fire-gem" ] && [ "$name" != "fire-log" ]; then
+    if [[ "$name" != "fire-gem" && "$name" != "fire-log" ]]; then
         nasm -f elf64 "$f" -o "VERSION 1/$name.o"
         ld "VERSION 1/$name.o" "VERSION 1/fire-log.o" -o "VERSION 1/$name.exe"
     fi
 done
 
-# Handoff to Version 1 Master Engine
+# 4. Launch the Executive
 "./VERSION 1/fire-gem.exe"
