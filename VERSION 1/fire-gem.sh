7f1287744165f35f687f1772e5ae20747a019f0a
CVBGOD <mercwar01@gmail.com>
2026-02-13 23:10:07 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# =============================================================================
-#  AVIS-SMITHY: VERSION 1 HARDCODE LOADER
-#  FILE: fire-gem.sh
-# =============================================================================
+# VERSION 1/fire-gem.sh
 set -e
 
-# 1. Forge Identity Artifacts
+# 1. Forge the Master Objects
 nasm -f elf64 "VERSION 1/fire-log.asm" -o "VERSION 1/fire-log.o"
 nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
 
-# 2. THE LINKER STRIKE
+# 2. LINKER STRIKE
 ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
 
-# 3. Ignition
+# 3. HANDOFF
 "./VERSION 1/fire-gem.exe"
