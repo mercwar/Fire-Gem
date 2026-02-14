07b1967b1cf458f181a0eb6c15e3c49de2ec84a4
CVBGOD <mercwar01@gmail.com>
2026-02-14 14:59:33 -0500

    Update AVIS_ALERT.js

--- htdocs/AVIS_FVS_ALERT/AVIS_ALERT.js
+++ htdocs/AVIS_FVS_ALERT/AVIS_ALERT.js
@@ -4,6 +4,7 @@ import { AVIS_ALERT } from "./AVIS_FVS_ALERT/AVIS_FVS_INI.js";
 /* ---------------------------------------------------------- */
 
 /* -------AVIS.FVS-CYBORG ARTIFACT: alert-------------------- */
+let MSG_TXT = true;
 let MSG_SYS_ON = true;
 let MSG_SYS_OFF = false;
 let MSG_TEST_ON = true;
@@ -12,9 +13,11 @@ let MSG_ALERT_ON  = true;
 let MSG_ALERT_OFF  = false;
 let MSG_NULL  = "";
 let MSG_ID  = "AVIS.FVS-CYBORG ARTIFACT";
+let MSG_AVIS_ALERT  = "";
 
-AVIS_ALERT(MSG_SYS_ON,MSG_TEST_ON,MSG_ALERT_OFF,MSG_NULL);
+MSG_AVIS_ALERT=AVIS_ALERT(MSG_SYS_ON,MSG_TEST_ON,MSG_ALERT_OFF,MSG_NULL,MSG_TXT);
 /* ---------------------------------------------------------- */
 /* -------AVIS.FVS-CYBORG ARTIFACT: alert-------------------- */
 // AVIS_ALERT(MSG_SHOW,MSG_TEST,MSG_ALERT,MSG_TEXT);
+
 /* ----------------------------------------------------------
