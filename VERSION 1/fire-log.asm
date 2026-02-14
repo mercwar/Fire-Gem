dfbbc66a6058fda1d21c57a24fa8b9bb2c393966
CVBGOD <mercwar01@gmail.com>
2026-02-14 00:02:18 -0500

    Update fire-log.asm

--- VERSION 1/fire-log.asm
+++ VERSION 1/fire-log.asm
@@ -1,13 +1,13 @@
-; =============================================================
-;  AVIS LLM-LOG ENCAPSULATOR [VERSION 1]
+; =============================================================================
+;  AVIS-CORE // LLM-LOG ENCAPSULATOR
 ;  FILE: fire-log.asm
-;  PURPOSE: Forge a Fancy Object for every Log Strike
-; =============================================================
+;  GOVERNANCE: CVBGOD // STATUS: HARDWARE_LOCK
+; =============================================================================
 
 section .data
-    log_path  db "VERSION 1/fire-gem.log", 0
-    avis_hdr  db "AVIS", 0x01          ; Magic + Protocol Ver
-    llm_tag   db "[LLM-LOG-OBJ]", 0    ; Fancy Object Identifier
+    log_path  db "VERSION 1/fire-log/fire-protocol.avis", 0
+    avis_hdr  db "AVIS", 0x01, 0x00          ; Magic + Ver
+    llm_tag   db "[LLM-LOG-OBJ]", 0x00      ; Fancy Object Tag
     tag_len   equ 14
 
 section .text
@@ -19,36 +19,36 @@ FIRE_LOG_STRIKE:
     push rdi            ; Save Body Pointer
     push rsi            ; Save Body Length
 
-    ; 1. OPEN LOG (VERSION 1 AUTHORITY)
-    mov rax, 2          ; sys_open
+    ; 1. OPEN VAULT (sys_open: 2)
+    mov rax, 2          
     mov rdi, log_path
     mov rsi, 1089       ; O_CREAT|WRONLY|APPEND
     mov rdx, 0644o
     syscall
     mov r12, rax        ; r12 = FD
 
-    ; 2. STRIKE AVIS MAGIC
-    mov rax, 1
+    ; 2. STRIKE AVIS MAGIC HEADER
+    mov rax, 1          ; sys_write
     mov rdi, r12
     mov rsi, avis_hdr
-    mov rdx, 5
+    mov rdx, 6
     syscall
 
-    ; 3. STRIKE FANCY LLM-LOG OBJECT TAG
+    ; 3. STRIKE LLM-LOG OBJECT SIGNATURE
     mov rax, 1
     mov rdi, r12
     mov rsi, llm_tag
     mov rdx, tag_len
     syscall
 
-    ; 4. STRIKE DATA BODY (The Proof)
+    ; 4. STRIKE PAYLOAD
     pop rdx             ; Restore Length
     pop rsi             ; Restore Pointer
-    mov rax, 1          ; sys_write
+    mov rax, 1
     mov rdi, r12
     syscall
 
-    ; 5. CLOSE
+    ; 5. CLOSE (sys_close: 3)
     mov rax, 3
     mov rdi, r12
     syscall
