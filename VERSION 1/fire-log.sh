52f30e3a0dc7281cb141ffd23e059714365d1838
CVBGOD <mercwar01@gmail.com>
2026-02-14 14:23:47 -0500

    Update fire-log.sh

--- VERSION 1/fire-log.sh
+++ VERSION 1/fire-log.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "[AVIS-YML-STRIKE] $(date -u): $1" >> "VERSION 1/fire-gem.log"
+# AVIS-LOG BRIDGE: WIPE ON INITIALIZATION | STRIKE ON CALL
+TXT="VERSION 1/fire-gem.txt"
+
+# IF CALL IS 'INIT', WIPE THE FILE. OTHERWISE, APPEND STRIKE.
+[ "$1" == "INIT" ] && echo "[AVIS-SH] $(date -u): SECTOR_CLEANED. HAHA!" > "$TXT" || echo "[AVIS-SH] $(date -u): $1" >> "$TXT"
