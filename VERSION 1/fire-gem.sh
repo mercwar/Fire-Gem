161e6396f270958645731a14f573ffec1b1734cc
CVBGOD <mercwar01@gmail.com>
2026-02-14 10:17:38 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -5,22 +5,35 @@
 # =============================================================
 set -e
 
-# LOG FUNCTION - THE SHELL'S VOICE
+# THE VOICE OF THE SHELL
 strike_log() {
     ./"VERSION 1/fire-log.sh" "[AVIS-SH] $1"
 }
 
-strike_log "SH_IGNITION: Sector VERSION 1 Secured."
+# 0. THE PURGE
+strike_log "PURGE_INIT: Cleaning Sector VERSION 1..."
+rm -f "VERSION 1/"*.o "VERSION 1/"*.exe
 
-# 1. FORGE VOICE
+# 1. FORGE CORE
+strike_log "CORE_FORGE: Seating Brain and Voice..."
 nasm -f elf64 "VERSION 1/fire-log.asm" -o "VERSION 1/fire-log.o"
-strike_log "VOICE_FORGED: fire-log.o Seated."
+nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
 
 # 2. LINK CORE
-# Fusing existing Gem with the new Voice
+strike_log "CORE_LINK: Fusing Triad into fire-gem.exe..."
 ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
-strike_log "CORE_LINKED: fire-gem.exe Ignited."
 
-# 3. INTERNAL HANDOFF
-strike_log "HANDOFF: Master Brain Seizing Control. HAHA!"
+# 3. RECURSIVE LOOP
+strike_log "RECURSIVE_FORGE: Sweeping extensions..."
+for f in "VERSION 1"/*.asm; do
+    name=$(basename "$f" .asm)
+    if [[ "$name" != "fire-gem" && "$name" != "fire-log" ]]; then
+        strike_log "EXT_STRIKE: Forging $name.o"
+        nasm -f elf64 "$f" -o "VERSION 1/$name.o"
+        ld "VERSION 1/$name.o" "VERSION 1/fire-log.o" -o "VERSION 1/$name.exe"
+    fi
+done
+
+# 4. IGNITION
+strike_log "IGNITION: Handoff to Master Executive. HAHA!"
 "./VERSION 1/fire-gem.exe"
