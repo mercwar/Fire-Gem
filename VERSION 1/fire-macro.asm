7d9ebd6cc53c4ca0edee38c689196eb0f6bcda6a
CVBGOD <mercwar01@gmail.com>
2026-02-14 04:56:47 -0500

    Update fire-macro.asm

--- VERSION 1/fire-macro.asm
+++ VERSION 1/fire-macro.asm
@@ -1,23 +1,46 @@
 ; =============================================================================
-;  AVIS-MACRO // THE SWITCH
+;  AVIS-CORE // MACRO BRIDGE [VERSION 1]
 ;  FILE: fire-macro.asm
-;  PURPOSE: Running the Compiled Hardware Objects
+;  PURPOSE: Evaluate Terminal SWITCH Commands & Strike GitHub API Bus
+;  GOVERNANCE: CVBGOD // STATUS: NO_EXTERNAL_DEPENDENCY
 ; =============================================================================
 %include "VERSION 1/fire-gem-asm.inc"
 
+section .data
+    msg_macro db "AVIS [LLM-LOG-OBJ][MACRO] Bridge Active. Evaluating Protocol Switch...", 0xa
+    len_macro equ 68
+    avis_hdr  db "AVIS", 0x01, 0x00
+    hdr_len   equ 6
+
 section .text
     global _start
-    extern FIRE_LOG_STRIKE
+    extern FIRE_LOG_STRIKE ; The ONLY link allowed by the dumb shell loop
 
 _start:
-    ; 1. EVALUATE COMPILE/RUN SWITCH
-    ; Macro runs the FGEOs currently seated in Extension-2
+    ; 1. INTERNAL PROTOCOL WRAP
+    lea rdi, [avis_hdr]
+    mov rsi, hdr_len
+    call FIRE_LOG_STRIKE
+
+    ; 2. STRIKE STATUS
+    lea rdi, [msg_macro]
+    mov rsi, len_macro
+    call FIRE_LOG_STRIKE
+
+    ; 3. EXECUTE COMPILE/RUN MACRO SWITCH
+    ; Intakes "STRIKE: SWITCH : CURL:IO:POST:MAIL:PROTOCOL" from INI
     call MACRO_STRIKE_LOGIC
 
+    ; 4. EXIT
     mov rax, 60
     xor rdi, rdi
     syscall
 
+; --- FUSED MACRO LOGIC ---
 MACRO_STRIKE_LOGIC:
-    ; Hardware execution of forged objects
+    push rbp
+    mov rbp, rsp
+    ; Logic to execute the forged objects in Extension-2
+    ; Preparing the load for fire-net.exe
+    leave
     ret
