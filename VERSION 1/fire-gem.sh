a76e5c13363b491fb86d90f74c4d8bc9b6761c11
CVBGOD <mercwar01@gmail.com>
2026-02-14 00:21:00 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-# VERSION 1/fire-gem.sh
+# =============================================================================
+#  AVIS-SMITHY: VERSION 1 HARDCODE LOADER [CVBGOD AUTHORITY]
+#  FILE: fire-gem.sh
+# =============================================================================
 set -e
 
-# 1. Forge the Encapsulator
+# 1. Forge the Global Voice (fire-log) and RPC Surface (fire-net)
 nasm -f elf64 "VERSION 1/fire-log.asm" -o "VERSION 1/fire-log.o"
-
-# 2. Forge and Link the Engine
+nasm -f elf64 "VERSION 1/fire-net.asm" -o "VERSION 1/fire-net.o"
 nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
-ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
 
-# 3. Forge the Rest of the Chain
+# 2. THE LINKER STRIKE: Fuse Brain, Voice, and Network
+ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-gem.exe"
+ld "VERSION 1/fire-net.o" "VERSION 1/fire-log.o" -o "VERSION 1/fire-net.exe"
+
+# 3. Forge the remaining Chain Artifacts
 for f in "VERSION 1"/*.asm; do
     name=$(basename "$f" .asm)
-    if [[ "$name" != "fire-gem" && "$name" != "fire-log" ]]; then
+    if [[ "$name" != "fire-gem" && "$name" != "fire-log" && "$name" != "fire-net" ]]; then
         nasm -f elf64 "$f" -o "VERSION 1/$name.o"
         ld "VERSION 1/$name.o" "VERSION 1/fire-log.o" -o "VERSION 1/$name.exe"
     fi
 done
 
-# 4. Ignition
+# 4. Handoff to Version 1 Master Engine
 "./VERSION 1/fire-gem.exe"
