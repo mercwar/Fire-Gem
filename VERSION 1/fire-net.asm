3c8b1b407654bacd033664f57cbfd160b4826c33
CVBGOD <mercwar01@gmail.com>
2026-02-14 00:48:00 -0500

    Update fire-net.asm

--- VERSION 1/fire-net.asm
+++ VERSION 1/fire-net.asm
@@ -1,40 +1,28 @@
-; =============================================================================
-;  AVIS-NET // RPC SURFACE [VERSION 1]
+; =============================================================
+;  AVIS-NET // RPC INTAKE
 ;  FILE: fire-net.asm
-; =============================================================================
+;  PURPOSE: Parse INI Command Objects & Feed Macro
+; =============================================================
 
 section .data
-    msg_net   db "AVIS [LLM-LOG-OBJ][AVIS-NET] RPC Surface: BROADCAST_START", 0xa
-    len_net   equ $ - msg_net
-    html_hdr  db "<avis-event class='proc-call'>", 0
-    html_ftr  db "</avis-event>", 0
+    ini_path db "VERSION 1/fire-gem.ini", 0
+
+section .bss
+    net_buf  resb 1024
 
 section .text
+    global FIRE_NET_INTAKE
     global _start
-    extern FIRE_LOG_STRIKE
 
 _start:
-    ; 1. STRIKE THE INITIAL RPC HANDSHAKE
-    lea rdi, [msg_net]
-    mov rsi, len_net
-    call FIRE_LOG_STRIKE
-
-    ; 2. SCAN INI FOR LOG_LEVEL (Logic to check GLOBAL_LEVEL=2)
-    ; If Level 2, we wrap in HTML Printouts
-    
-    ; 3. THE HTML PRINTOUT STRIKE
-    lea rdi, [html_hdr]
-    mov rsi, 29
-    call FIRE_LOG_STRIKE   ; Every proc call now gets an HTML wrapper
-
-    ; [PROCEDURE CALL LOGIC HERE]
-    ; Every sub-process strike is now a mapped AVIS object
-
-    lea rdi, [html_ftr]
-    mov rsi, 13
-    call FIRE_LOG_STRIKE
-
+    ; (Terminal Entry Point for Net Surface)
+    call FIRE_NET_INTAKE
     mov rax, 60
     xor rdi, rdi
     syscall
 
+FIRE_NET_INTAKE:
+    ; 1. OPEN INI & EXTRACT STRIKE_01 (CURL DATA)
+    ; 2. RETURN BUFFER POINTER IN RDI
+    lea rdi, [net_buf]
+    ret
