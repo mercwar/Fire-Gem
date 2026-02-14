3fc8f71bdda1bf8574418236bbca91c55c6f4eed
CVBGOD <mercwar01@gmail.com>
2026-02-14 14:11:24 -0500

    Update fire-link.sh

--- VERSION 1/fire-link.sh
+++ VERSION 1/fire-link.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # =============================================================
 #  AVIS-LINKER // EXPORT FORGE [VERSION 1]
-#  FILE: fire-link.sh
 # =============================================================
 set -e
 
-TXT_FILE=$(grep "TXT_AUDIT=" "VERSION 1/fire-gem.ini" | cut -d'=' -f2)
+# LOG TO TXT
+./"VERSION 1/fire-log.sh" "LINK_STRIKE: Resolving _start Collision."
 
-# FORGE STRIKE
-echo "[SH-STRIKE] $(date -u): LINKER_FORGE_START" >> "$TXT_FILE"
+# THE SUPREME FUSION
+# Linking Gem (The _start) + Compile (The Function) + Log (The Voice)
+ld "VERSION 1/fire-gem.o" \
+   "VERSION 1/fire-log.o" \
+   "VERSION 1/fire-compile.o" \
+   -o "VERSION 1/fire-gem.exe"
 
-nasm -f elf64 "VERSION 1/fire-log.asm" -o "VERSION 1/fire-log.o"
-nasm -f elf64 "VERSION 1/fire-gem.asm" -o "VERSION 1/fire-gem.o"
-nasm -f elf64 "VERSION 1/fire-compile.asm" -o "VERSION 1/fire-compile.o"
-
-# SUPREME LINK
-ld "VERSION 1/fire-gem.o" "VERSION 1/fire-log.o" "VERSION 1/fire-compile.o" -o "VERSION 1/fire-gem.exe"
-
-echo "[SH-STRIKE] $(date -u): LINKER_FORGE_COMPLETE" >> "$TXT_FILE"
+./"VERSION 1/fire-log.sh" "LINK_COMPLETE: _start Conflict Dead. HAHA!"
