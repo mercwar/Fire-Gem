bb3a0e666a497f8df1a7e42fbde1e66d4f6902f9
CVBGOD <mercwar01@gmail.com>
2026-02-14 00:21:58 -0500

    Update fire-log.asm

--- VERSION 1/fire-log.asm
+++ VERSION 1/fire-log.asm
@@ -1,14 +1,9 @@
-; =============================================================================
-;  AVIS-CORE // LLM-LOG ENCAPSULATOR
-;  FILE: fire-log.asm
-;  GOVERNANCE: CVBGOD // STATUS: HARDWARE_LOCK
-; =============================================================================
-
+; VERSION 1/fire-log.asm
 section .data
-    log_path  db "VERSION 1/fire-log/fire-protocol.avis", 0
-    avis_hdr  db "AVIS", 0x01, 0x00          ; Magic + Ver
-    llm_tag   db "[LLM-LOG-OBJ]", 0x00      ; Fancy Object Tag
-    tag_len   equ 14
+    log_path db "VERSION 1/fire-log/fire-protocol.avis", 0
+    avis_hdr db "AVIS [LLM-LOG-OBJ]", 0
+    html_pre db "<avis-obj data-lvl='2'>", 0
+    html_suf db "</avis-obj>", 0xa, 0
 
 section .text
     global FIRE_LOG_STRIKE
@@ -16,42 +11,40 @@ section .text
 FIRE_LOG_STRIKE:
     push rbp
     mov rbp, rsp
-    push rdi            ; Save Body Pointer
-    push rsi            ; Save Body Length
+    push rdi
+    push rsi
 
-    ; 1. OPEN VAULT (sys_open: 2)
-    mov rax, 2          
+    ; 1. OPEN VAULT
+    mov rax, 2          ; open
     mov rdi, log_path
     mov rsi, 1089       ; O_CREAT|WRONLY|APPEND
     mov rdx, 0644o
     syscall
-    mov r12, rax        ; r12 = FD
+    mov r12, rax
 
-    ; 2. STRIKE AVIS MAGIC HEADER
-    mov rax, 1          ; sys_write
-    mov rdi, r12
-    mov rsi, avis_hdr
-    mov rdx, 6
-    syscall
-
-    ; 3. STRIKE LLM-LOG OBJECT SIGNATURE
+    ; 2. STRIKE HTML WRAPPER (Start)
     mov rax, 1
     mov rdi, r12
-    mov rsi, llm_tag
-    mov rdx, tag_len
+    mov rsi, html_pre
+    mov rdx, 23
     syscall
 
-    ; 4. STRIKE PAYLOAD
-    pop rdx             ; Restore Length
-    pop rsi             ; Restore Pointer
+    ; 3. STRIKE BODY
+    pop rdx
+    pop rsi
     mov rax, 1
     mov rdi, r12
     syscall
 
-    ; 5. CLOSE (sys_close: 3)
-    mov rax, 3
+    ; 4. STRIKE HTML WRAPPER (End)
+    mov rax, 1
     mov rdi, r12
+    mov rsi, html_suf
+    mov rdx, 12
     syscall
 
+    mov rax, 3          ; close
+    mov rdi, r12
+    syscall
     leave
     ret
