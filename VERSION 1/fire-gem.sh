cef2a931e428664be5f5ea0e6035232b2c1c5ac2
CVBGOD <mercwar01@gmail.com>
2026-02-14 14:27:46 -0500

    Update fire-gem.sh

--- VERSION 1/fire-gem.sh
+++ VERSION 1/fire-gem.sh
@@ -2,6 +2,7 @@
 set -e
 export TERM=xterm
 INI="VERSION 1/fire-gem.ini"
+# QUOTE THE INTAKE
 TXT=$(grep "TXT_AUDIT=" "$INI" | cut -d'=' -f2)
 
 # PROFESSIONAL AVIS PROGRESS BAR
@@ -9,15 +10,14 @@ avis_progress() {
     local label=$1
     echo -ne "\r[AVIS] $label [░░░░░░░░░░] 0%"
     sleep 0.1
-    echo -ne "\r[AVIS] $label [▓▓▓▓▓▓░░░░] 60%"
-    sleep 0.1
     echo -ne "\r[AVIS] $label [▓▓▓▓▓▓▓▓▓▓] 100% DONE\n"
-    ./VERSION 1/fire-log.sh "$label COMPLETE"
+    # QUOTED BRIDGE CALL
+    "./VERSION 1/fire-log.sh" "$label COMPLETE"
 }
 
 # 1. ATOMIC PURGE
 chmod +x "VERSION 1/fire-log.sh"
-./VERSION 1/fire-log.sh "INIT"
+"./VERSION 1/fire-log.sh" "INIT"
 
 # 2. VISUAL STRIKES
 clear
@@ -38,5 +38,3 @@ avis_progress "MASTER_BRAIN_IGNITION"
 # 3. AUDIT DUMP
 echo "--- FINAL AUDIT [$TXT] ---"
 cat "$TXT"
-
-
