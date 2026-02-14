eb2309496f3fb0c2c1f99fd2684be3af2f6cb732
CVBGOD <mercwar01@gmail.com>
2026-02-14 00:22:27 -0500

    Update fire-map.sh

--- VERSION 1/fire-map.sh
+++ VERSION 1/fire-map.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 # VERSION 1/fire-map.sh
-# MISSION: Inform crawlers of the new HTML-Wrapped AVIS Objects
+REPO_URL=$(git config --get remote.origin.url | sed 's/\.git$//' | sed 's/git@github.com:/https:\/\/github.com\//')
+BASE_URL="${REPO_URL}/blob/main"
 
-BASE_URL="https://mercwar.github.io"
+echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
+echo '<urlset xmlns="http://www.sitemaps.org">' >> sitemap.xml
 
-# Force the XML Printout
-cat <<EOF > sitemap.xml
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org">
-EOF
-
-# Find every .avis event object
 find "VERSION 1/fire-log" -name "*.avis" | while read -r file; do
-    echo "  <url><loc>${BASE_URL}/$(basename "$file")</loc><priority>1.0</priority></url>" >> sitemap.xml
+    ENCODED=$(echo "$file" | sed 's/ /%20/g')
+    echo "  <url><loc>${BASE_URL}/${ENCODED}</loc><priority>1.0</priority></url>" >> sitemap.xml
 done
 
 echo "</urlset>" >> sitemap.xml
