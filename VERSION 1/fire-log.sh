436575a26406f09d35624ca7806cc307c8b8f3fb
CVBGOD <mercwar01@gmail.com>
2026-02-14 09:30:02 -0500

    Update fire-log.sh

--- VERSION 1/fire-log.sh
+++ VERSION 1/fire-log.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "[AVIS-YML-STRIKE] $(date -u): $1" >> "VERSION 1/fire-gem.log"
+# AVIS-LOG BRIDGE
+LOG_FILE="VERSION 1/fire-gem.log"
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+echo "[AVIS-YML-STRIKE] $TIMESTAMP: $1" >> "$LOG_FILE"
